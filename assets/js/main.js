ad6d4e820f6365ba0eac79f5bc6055a4bff00a89
jinclops <130402488+tektite-tv@users.noreply.github.com>
2025-10-17 19:49:39 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,15 @@
+// /assets/js/main.js
+import { loadPosts } from "./posts.js";
+import { loadVideos } from "./videos.js";
+import { setupSearch } from "./search.js";
+import { activateLightbox } from "./lightbox.js";
+import "./commands.js";
+import "./eastereggs.js";
 
+document.addEventListener("DOMContentLoaded", async () => {
+  console.log("Temple booting...");
+  await loadPosts();
+  await loadVideos();
+  activateLightbox();
+  setupSearch();
+});
