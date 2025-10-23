e4acd22a1cd0a2d96232e2d34c28a4ab1cfa11ca
jinclops <130402488+tektite-tv@users.noreply.github.com>
2025-10-22 21:49:51 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,13 +4,16 @@ import { buildIndex } from "./indexBuilder.js";
 import { initSearch } from "./search.js";
 import { initContact } from "./contact.js";
 import { initMenu } from "./menu.js";
+import { initLightbox } from "./lightbox.js";
+import { initPostPopup } from "./postPopup.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
-  console.log("Initializing site...");
   await loadPosts();
   await loadVideos();
   await buildIndex();
   initSearch();
   initContact();
   initMenu();
+  initLightbox();
+  initPostPopup();
 });
