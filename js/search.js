5582317914e57f3963911b6fbbe0aa7fababe0c4
jinclops <130402488+tektite-tv@users.noreply.github.com>
2025-10-22 21:45:14 -0400

    Update search.js

--- js/search.js
+++ js/search.js
@@ -7,9 +7,9 @@ export function initSearch() {
 
   const renderPlaceholder = () => {
     popup.innerHTML = `
-  <div class="promptline">theinternetisdead.org</div>
-  <div class="search-result">Type to search posts/videos, or try commands: <b>/help</b></div>
-`;
+      <div class="promptline">theinternetisdead.org</div>
+      <div class="search-result">Type to search posts/videos, or try commands: <b>/help</b></div>
+    `;
   };
 
   const openPopup = () => {
