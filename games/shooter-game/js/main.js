5a20def579700037d3d7d6ed9149323b8d12fe2e
jinclops <130402488+tektite-tv@users.noreply.github.com>
2025-10-29 20:28:07 -0400

    Update main.js

--- games/shooter-game/js/main.js
+++ games/shooter-game/js/main.js
@@ -1,1 +1,10 @@
+import { setupUI, startGame } from "./ui.js";
+import { initGameLoop } from "./game.js";
 
+window.addEventListener("DOMContentLoaded", () => {
+  setupUI();
+  initGameLoop();
+  console.log("Bananarama Apocalypse ready.");
+});
+
+export { startGame };
