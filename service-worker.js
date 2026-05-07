faff28d2d3a118426fbc2cd05b98b0d9dcae0f0d
jinclops <130402488+tektite-tv@users.noreply.github.com>
2026-05-06 20:33:58 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -46,7 +46,7 @@ self.addEventListener('fetch', (event) => {
   );
   const isVideoPathRoute = /^\/dead\/videos\/[^/]+\/?$/.test(url.pathname);
   const isVideoQueryRoute = /^\/dead\/videos\/?$/.test(url.pathname)
-    && (url.searchParams.has('video_embed') || url.searchParams.has('vid') || url.searchParams.has('id'));
+    && (url.searchParams.has('vid') || url.searchParams.has('id'));
   if (!isExperimentShortPath && !isVideoPathRoute && !isVideoQueryRoute) return;
 
   event.respondWith(fetch('/index.html', { cache: 'no-store' }));
